db03bf0bcba4ac462a7fa89ed8120c0832add88d
Michal <michal.r982@stu.nebo.edu>
2018-09-10 10:10:49 -0600

    updated changes

--- 2D project/Assets/Scrips/char move.cs
+++ 2D project/Assets/Scrips/char move.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class charmove : MonoBehaviour {
 
+    // Player Movement Variables
+    public int MoveSpeed;
+    public float jumpheight;
+
+    // Player grounded variables
+    public Transform groundcheck;
+    public float groundCheckRadius;
+    public LayerMask whatisground;
+    private bool grounded;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
+
+
+	private void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, whatisground)
+	}
+
+
 	// Update is called once per frame
 	void Update () {
-		
+
+        //this code makes the character jump
+        if(Input.GetKeyDown (KeyCode.Space)&& grounded)
+            Jump();
 	}
+
+
+    public void Jump(){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpheight);
+    }
 }
