0118590119affb699de1fbcece7e433bd27e97bc
Michal <michal.r982@stu.nebo.edu>
2018-09-24 10:08:48 -0600

    added DeathBox

---
+++ 2D project/Assets/Scrips/Deathbox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deathbox : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D other){
+
+        if(other.name == "PC"){
+
+            Debug.Log("player enters death zone");
+            Destroy(other);
+        }
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
