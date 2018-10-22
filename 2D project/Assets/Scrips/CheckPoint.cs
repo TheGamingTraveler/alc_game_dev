3834b8f8ab12b9a70e86289b587c90a937205d15
Michal <michal.r982@stu.nebo.edu>
2018-10-22 10:07:55 -0600

    updated killplayer script

--- 2D project/Assets/Scrips/CheckPoint.cs
+++ 2D project/Assets/Scrips/CheckPoint.cs
@@ -8,12 +8,20 @@ public class CheckPoint : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        levelManager = FindObjectOfType<LevelManager>();
         
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+       
+        }
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.name = "PC"){
+            levelManager.currentCheckPoint = gameObject;
+            Debug.Log("Ativated CheckPoint" + transform.position);
+        }
 		
 	}
 }
